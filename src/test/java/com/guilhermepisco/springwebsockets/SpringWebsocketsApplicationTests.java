d1ae6e0ef937ca8869bb023f681941823ad76249
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2020-07-17 00:11:27 +0100

    Empty project creation

---
+++ src/test/java/com/guilhermepisco/springwebsockets/SpringWebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermepisco.springwebsockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
